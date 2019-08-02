726d9c417289dbd2f22a264f749a9a80f288dad4
niuniu <niuniu_lz@163.com>
2019-08-02 16:02:28 +0800

    pages

---
+++ build/precache-manifest.e3b4b74bdb1d6faf1a4a27798f9cbb33.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67769df90d9dbb7078b6479da465ef4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d1fbfea068fcec6bed2b",
+    "url": "/static/css/main.2ded7b1e.chunk.css"
+  },
+  {
+    "revision": "72c252c37735d2180df8",
+    "url": "/static/js/2.c2695c00.chunk.js"
+  },
+  {
+    "revision": "d1fbfea068fcec6bed2b",
+    "url": "/static/js/main.02909765.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
